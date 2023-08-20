88d25cd088cc556d9916ea997d2714056d56d908
dimas <dfebri446@gmail.com>
2023-08-20 23:53:20 +0700

    add tailwind class sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div className='flex h-screen w-screen items-center justify-center'>
+            <h1 className='text-5xl'>App</h1>
+        </div>
+    );
 };
 
 export default App;
